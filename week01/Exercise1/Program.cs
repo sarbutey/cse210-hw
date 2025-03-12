dac47d80b8f846262ccfaa36402a1206f24998f7
sarbutey <sar24005@byui.edu>
2025-03-12 04:02:36 +0000

    finished execise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Prompt the user for their first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Prompt the user for their last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Display the formatted text
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
