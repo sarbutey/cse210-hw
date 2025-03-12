157268cce78b5854a010a55229c3150f60194dae
sarbutey <sar24005@byui.edu>
2025-03-12 04:30:50 +0000

    finish execise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+         // Prompt the user for their grade percentage
+        Console.Write("Enter your grade percentage: ");
+        int score = int.Parse(Console.ReadLine());
+
+        // Determine the letter grade
+        string letter;
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+        else if (score >= 80)
+        {
+            letter = "B";
+        }
+        else if (score >= 70)
+        {
+            letter = "C";
+        }
+        else if (score >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Print the letter grade
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        // Determine if the user passed the course
+        if (score >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you did not pass the course. Better luck next time!");
+        }
     }
 }
