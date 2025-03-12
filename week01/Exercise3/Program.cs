c211d614a19c43e44318b31b3ef3237aab95c6dc
sarbutey <sar24005@byui.edu>
2025-03-12 04:46:51 +0000

    excise 3 done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+       // Initialize the random number generator
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        // Prompt the user for a guess
+        int guess = -1;
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            // Determine if the user needs to guess higher or lower
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
